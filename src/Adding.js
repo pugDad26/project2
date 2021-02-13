4e1edbb8e1ac7de33f314cde3d6e4091d38a7e32
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-02-13 17:04:42 -0600

    created an adding component

---
+++ src/Adding.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Adding = (props) => {
+    return (
+        <h2>
+            {props.numberOne} + {props.numberTwo} = {props.numberOne + props.numberTwo} 
+        </h2>
+    );
+};
